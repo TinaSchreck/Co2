0a6c15600f07865d776a9cb91c4d8231e0467c09
TinaSchreck <164375095+TinaSchreck@users.noreply.github.com>
2024-06-20 13:11:42 +0200

    Update streamlit_app_5.py

--- streamlit_app_5.py
+++ streamlit_app_5.py
@@ -6,7 +6,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import os
 from joblib import dump, load
 from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.tree import DecisionTreeRegressor
